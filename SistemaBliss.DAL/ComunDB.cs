fa653641d4bb34184a9b79aec6bd5d08053d2ebc
Mario Enrique Calzadilla Sintigo <marioecsintigo@outlook.com>
2024-08-08 15:27:32 -0600

    Se realizo la conexión al sistema de base de datos "SistemaBliss" en SQLSERVER

---
+++ SistemaBliss.DAL/ComunDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SistemaElParaisal.DAL
+{
+    public class ComunDB
+    {
+        const string strConexion = @"Data Source=MarioSintigo\SQLEXPRESS;Initial Catalog=SistemaBlissBD;Integrated Security=True;Trust Server Certificate=True";
+
+        private static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(strConexion);
+            conexion.Open();
+            return conexion;
+        }
+
+        public static SqlCommand ObtenerComando()
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = ObtenerConexion();
+            return comando;
+        }
+
+        public static SqlCommand ObtenerComando(SqlTransaction pTransaction)
+        {
+            // Sobre carga del metodo ObtenerComando para crear comandos de transaccion
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = pTransaction.Connection;
+            comando.Transaction = pTransaction;
+            return comando;
+        }
+
+        public static int EjecutarComando(SqlCommand pComando)
+        {
+            int resultado = pComando.ExecuteNonQuery();
+            pComando.Connection.Close();
+            return resultado;
+        }
+
+        public static SqlDataReader EjecutarComandoReader(SqlCommand pComando)
+        {
+            SqlDataReader reader = pComando.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+
+        public static SqlTransaction CrearTransaction()
+        {
+            SqlConnection conexion = ObtenerConexion();
+            return conexion.BeginTransaction();
+        }
+    }
+}
