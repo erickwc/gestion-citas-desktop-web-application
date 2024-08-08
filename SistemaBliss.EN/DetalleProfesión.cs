94d9a156957682add10f5b3dea42b57131e15a0e
Mario Enrique Calzadilla Sintigo <marioecsintigo@outlook.com>
2024-08-08 17:00:42 -0600

    Se agregaron las funciones en "DetalleProfesion" y "Profesion" en "SistemaBliss.EN"

--- SistemaBliss.EN/DetalleProfesión.cs
+++ SistemaBliss.EN/DetalleProfesión.cs
@@ -8,5 +8,20 @@ namespace SistemaBliss.EN
 {
    public class DetalleProfesión
     {
+        public int IdDetalleProfesion
+        { get; set; }
+
+        public int IdUsuario
+        { get; set; }
+
+        public int IdProfesion
+        { get; set; }
+
+        public virtual Usuario Usuario
+        { get; set; }
+
+        public virtual Profesión Profesión { get; set; }
+
+
     }
 }
