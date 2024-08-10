ff7746379102f42bf72154a1ca09a36244659905
Mario Enrique Calzadilla Sintigo <marioecsintigo@outlook.com>
2024-08-09 22:28:20 -0600

    Se progamaron los metodos de la clase "RolDAL" en la capa "SistemaBliss.DAL"

--- SistemaBliss.DAL/RolDAL.cs
+++ SistemaBliss.DAL/RolDAL.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//referencias del proyecto
+using SistemaBliss.EN;
+
+
+using SistemaElParaisal.DAL;
 
 namespace SistemaBliss.DAL
 {
     public class RolDAL
     {
+        #region Metodos de Busqueda
+        public static Rol ObtenerPorId(byte pIdRol)
+        {
+            Rol obj = new Rol();
+
+            SqlCommand comando = ComunDB.ObtenerComando();
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_ObtenerRolPorId";
+            comando.Parameters.AddWithValue("@IdRol", pIdRol);
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(comando);
+            while (reader.Read())
+            {
+                // Orden de las columnas depende de la Consulta SELECT utilizada
+                obj.IdRol = reader.GetByte(0); // Columna [0] cero
+                obj.Nombre = reader.GetString(1);  // Columna [1] uno
+            }
+            return obj;
+        }
+        #endregion
+        public static List<Rol> Buscar(Rol pRol)
+        {
+            List<Rol> lista = new List<Rol>();
+
+            #region Proceso
+            using (SqlCommand comando = ComunDB.ObtenerComando())
+            {
+                byte contador = 0;
+                string whereSQL = " ";
+                string consulta = @"SELECT TOP 100 c.IdCargo, c.Nombre
+                            FROM Cargo c ";
+
+                // Validar filtros
+                if (pRol.Nombre != null && pRol.Nombre.Trim() != string.Empty)
+                {
+                    if (contador > 0)
+                        whereSQL += " AND ";
+                    contador += 1;
+                    whereSQL += " c.Nombre LIKE @Nombre ";
+                    comando.Parameters.AddWithValue("@Nombre", "%" + pRol.Nombre + "%");
+                }
+                // Agregar filtros
+                if (whereSQL.Trim().Length > 0)
+                {
+                    whereSQL = " WHERE " + whereSQL;
+                }
+                comando.CommandText = consulta + whereSQL;
+
+                SqlDataReader reader = ComunDB.EjecutarComandoReader(comando);
+                while (reader.Read())
+                {
+                    Rol obj = new Rol();
+                    // Orden de las columnas depende de la Consulta SELECT utilizada
+                    obj.IdRol = reader.GetByte(0);
+                    obj.Nombre = reader.GetString(1);
+                    lista.Add(obj);
+                }
+                comando.Connection.Dispose();
+            }
+            #endregion
+            return lista;
+        }
+
+
     }
 }
