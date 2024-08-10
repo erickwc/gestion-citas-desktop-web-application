7a8a54060a8648e0f0f86fdad5effbdb21801f98
Michelle Figueroa <michellefigueroa335@outlook.com>
2024-08-09 22:42:54 -0600

    Se programaron los métodos de la clase ClienteDAL" en la capa SistemaBliss.DAL

--- SistemaBliss.DAL/ClienteDAL.cs
+++ SistemaBliss.DAL/ClienteDAL.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using SistemaElParaisal.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaBliss.EN;
+
 
 namespace SistemaBliss.DAL
 {
     public class ClienteDAL
     {
+        #region Metodos de Busqueda
+        public static Cliente ObtenerPorId(byte pIdCliente)
+        {
+            Cliente obj = new Cliente();
+
+            SqlCommand comando = ComunDB.ObtenerComando();
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_ObtenerCargoPorId";
+            comando.Parameters.AddWithValue("@IdCargo", pIdCliente);
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(comando);
+            while (reader.Read())
+            {
+                // Orden de las columnas depende de la Consulta SELECT utilizada
+                obj.IdCliente = reader.GetByte(0); // Columna [0] cero
+                obj.IdUsuario = reader.GetByte(1);  // Columna [1] uno
+            }
+            return obj;
+        }
+        #endregion
     }
 }
