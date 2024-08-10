a01194f21c2cc8ddb849b2668d046c5c06e285fd
Erick Caceres <erickcaceres1717@outlook.com>
2024-08-09 22:48:22 -0600

    Codificacion de los metodos en la clase EmpresaDal de la Capa SistemaBliss.DAL

--- SistemaBliss.DAL/EmpresaDAL.cs
+++ SistemaBliss.DAL/EmpresaDAL.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using SistemaElParaisal.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaBliss.EN;
 
 namespace SistemaBliss.DAL
 {
     public class EmpresaDAL
     {
+        #region Metodo MODIFICAR
+        public static int Modificar(Empresa pEmpresa)
+        {
+            SqlCommand comando = ComunDB.ObtenerComando();
+            comando.CommandText = "SP_ModificarInformacionEmpresa";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@IdEmpresa", pEmpresa.IdEmpresa);
+            comando.Parameters.AddWithValue("@Nombre", pEmpresa.Nombre);
+            comando.Parameters.AddWithValue("@Telefono", pEmpresa.Telefono);
+            comando.Parameters.AddWithValue("@Direccion", pEmpresa.Direccion);
+            comando.Parameters.AddWithValue("@CorreoElectronico", pEmpresa.CorreoElectronico);
+            return ComunDB.EjecutarComando(comando);
+        }
+
+        #region Metodos de Busqueda
+        public static Empresa ObtenerPorId(byte pIdEmpresa)
+        {
+            Empresa obj = new Empresa();
+
+            SqlCommand comando = ComunDB.ObtenerComando();
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_ObtenerEmpresaPorId";        
+
+            SqlDataReader reader = ComunDB.EjecutarComandoReader(comando);
+            while (reader.Read())
+            {
+                obj.IdEmpresa = reader.GetByte(0); 
+                obj.Nombre = reader.GetString(1); 
+                obj.Telefono = reader.GetString(2); 
+                obj.Direccion = reader.GetString(3); 
+                obj.CorreoElectronico = reader.GetString(4);  
+            }
+            return obj;
+        }
+        #endregion
+
+        #endregion
     }
 }
