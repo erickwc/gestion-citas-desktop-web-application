36123a88422446f347c9bdccdfb69bb46a833d07
Erick Caceres <erickcaceres1717@outlook.com>
2024-08-11 23:01:49 -0600

    Codificacion de los metodos en las clases Empresa y HorariosEmpresa en la capa SistemaBliss.BL

--- SistemaBliss.BL/EmpresaBL.cs
+++ SistemaBliss.BL/EmpresaBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaBliss.DAL;
+using SistemaBliss.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace SistemaBliss.BL
 {
     public class EmpresaBL
     {
+        public int Modificar(Empresa pEmpresa)
+        {
+            return EmpresaDAL.Modificar(pEmpresa);
+        }
+
+        public Empresa ObtenerPorId(byte pIdEmpresa)
+        {
+            return EmpresaDAL.ObtenerPorId(pIdEmpresa);
+        }
+
     }
 }
--- SistemaBliss.BL/HorariosEmpresaBL.cs
+++ SistemaBliss.BL/HorariosEmpresaBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaBliss.DAL;
+using SistemaBliss.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace SistemaBliss.BL
 {
     public class HorariosEmpresaBL
     {
+        public int Guardar(HorariosEmpresa pHorariosEmpresa)
+        {
+            return HorariosEmpresaDAL.Guardar(pHorariosEmpresa);
+        }
+
+        public int Modifiar(HorariosEmpresa pHorariosEmpresa)
+        {
+            return HorariosEmpresaDAL.Modificar(pHorariosEmpresa);
+        }
+
+        public int Eliminar(HorariosEmpresa pHorariosEmpresa)
+        {
+            return HorariosEmpresaDAL.Eliminar(pHorariosEmpresa);
+        }
+
+        public static void ObtenerHorarios(HorariosEmpresa pHorariosEmpresa)
+        {
+            HorariosEmpresaDAL.ObtenerHorariosEmpresa();
+        }
     }
 }
