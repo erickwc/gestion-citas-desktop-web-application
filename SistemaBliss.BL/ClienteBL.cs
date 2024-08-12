8c3b64733576500cf1f0d73fee4e32e2dc0d3dce
Michelle Figueroa <michellefigueroa335@outlook.com>
2024-08-12 00:44:12 -0600

    Codificacion de los metodos en las clases ClienteBL y ServicioBL en la capa SistemaBliss.BL

--- SistemaBliss.BL/ClienteBL.cs
+++ SistemaBliss.BL/ClienteBL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaBliss.DAL;
+using SistemaBliss.EN;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,14 @@ namespace SistemaBliss.BL
 {
     public class ClienteBL
     {
+      
+
+        public Cliente ObtenerPorId(byte pIdCliente)
+        {
+            return ClienteDAL.ObtenerPorId(pIdCliente);
+        }
+        
+        
     }
+
 }
--- SistemaBliss.BL/ServicioBL.cs
+++ SistemaBliss.BL/ServicioBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaBliss.DAL;
+using SistemaBliss.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,26 @@ namespace SistemaBliss.BL
 {
     public class ServicioBL
     {
+
+        public int Guardar(Servicio pServicio)
+        {
+            return ServicioDAL.Guardar(pServicio);
+        }
+
+        public int Modificar(Servicio pServicio)
+        {
+            return ServicioDAL.Modificar(pServicio);
+        }
+
+        public int Eliminar(Servicio pServicio)
+        {
+            return ServicioDAL.Eliminar(pServicio);
+        }
+
+        public static Servicio ObtenerPorId(short pServicio)
+        {
+           return ServicioDAL.ObtenerPorId(pServicio);
+        }
     }
 }
+
