aa7ec4ffdc6fc12be4076f777ca93bb22f059ea0
erickwc <erickcaceres1717@gmail.com>
2024-08-14 02:37:32 -0600

    Codificacion de metodos y creacion de clase en las pruebas unitarias

---
+++ SistemaBliss.PruebasUnitarias/EstadoUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SistemaBliss.PruebasUnitarias
+{
+    [TestClass]
+    public class EstadoUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
