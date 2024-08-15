b950177735dd660ea7689235f2e4a64cc8fd23b3
Mario Enrique Calzadilla Sintigo <marioecsintigo@outlook.com>
2024-08-15 15:59:11 -0600

    Inicio del diseño del login

--- SistemaBliss.UI.WinForms/Login/Administracion/AdminLoginForm.Designer.cs
+++ SistemaBliss.UI.WinForms/Login/Administracion/AdminLoginForm.Designer.cs
@@ -28,19 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(49)))));
+            this.label1.Location = new System.Drawing.Point(500, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hola, Bienvenido de nuevo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.label2.Location = new System.Drawing.Point(432, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(530, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa los siguientes datos para iniciar sesión";
             // 
             // AdminLoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(805, 451);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1348, 701);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminLoginForm";
-            this.Text = "AdminLoginForm";
+            this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
