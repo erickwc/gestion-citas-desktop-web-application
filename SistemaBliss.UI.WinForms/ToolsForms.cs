c2f00a10a6699da12a16769f342841a03169b085
Erick Caceres <erickcaceres1717@outlook.com>
2024-08-15 14:05:21 -0600

    Creacion de la clase ToolsForms en la capa UI

---
+++ SistemaBliss.UI.WinForms/ToolsForms.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBliss.UI.WinForms
+{
+    internal class ToolsForms
+    {
+    }
+}
