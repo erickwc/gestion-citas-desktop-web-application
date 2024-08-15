b2dd5c3915fb684e4c848c986e125932171b45dc
michellefigueroa335@outlook.com <jonatan132012>
2024-08-15 16:32:58 -0600

    Aignación de nombres a cada elemento del formulario Login

--- SistemaBliss.UI.WinForms/Login/Administracion/AdminLoginForm.Designer.cs
+++ SistemaBliss.UI.WinForms/Login/Administracion/AdminLoginForm.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.SaludoLoginLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.InstruccionLabel = new System.Windows.Forms.Label();
             this.TelefonoLoginLabel = new System.Windows.Forms.Label();
             this.TelefonoLoginTextBox = new Guna.UI2.WinForms.Guna2TextBox();
-            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.ContrasenaTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.ContrasenaLabel = new System.Windows.Forms.Label();
+            this.IniciarSesionButton = new Guna.UI2.WinForms.Guna2Button();
             this.SuspendLayout();
             // 
             // SaludoLoginLabel
@@ -43,23 +43,25 @@
             this.SaludoLoginLabel.AutoSize = true;
             this.SaludoLoginLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaludoLoginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(49)))));
-            this.SaludoLoginLabel.Location = new System.Drawing.Point(492, 86);
+            this.SaludoLoginLabel.Location = new System.Drawing.Point(369, 70);
+            this.SaludoLoginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SaludoLoginLabel.Name = "SaludoLoginLabel";
-            this.SaludoLoginLabel.Size = new System.Drawing.Size(391, 37);
+            this.SaludoLoginLabel.Size = new System.Drawing.Size(313, 30);
             this.SaludoLoginLabel.TabIndex = 0;
             this.SaludoLoginLabel.Text = "Hola, Bienvenido de nuevo";
             // 
-            // label2
+            // InstruccionLabel
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
-            this.label2.Location = new System.Drawing.Point(424, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(530, 30);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Ingresa los siguientes datos para iniciar sesión";
+            this.InstruccionLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.InstruccionLabel.AutoSize = true;
+            this.InstruccionLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstruccionLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.InstruccionLabel.Location = new System.Drawing.Point(318, 110);
+            this.InstruccionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.InstruccionLabel.Name = "InstruccionLabel";
+            this.InstruccionLabel.Size = new System.Drawing.Size(440, 25);
+            this.InstruccionLabel.TabIndex = 1;
+            this.InstruccionLabel.Text = "Ingresa los siguientes datos para iniciar sesión";
             // 
             // TelefonoLoginLabel
             // 
@@ -67,9 +69,10 @@
             this.TelefonoLoginLabel.AutoSize = true;
             this.TelefonoLoginLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TelefonoLoginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(49)))));
-            this.TelefonoLoginLabel.Location = new System.Drawing.Point(465, 215);
+            this.TelefonoLoginLabel.Location = new System.Drawing.Point(349, 175);
+            this.TelefonoLoginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TelefonoLoginLabel.Name = "TelefonoLoginLabel";
-            this.TelefonoLoginLabel.Size = new System.Drawing.Size(95, 27);
+            this.TelefonoLoginLabel.Size = new System.Drawing.Size(77, 21);
             this.TelefonoLoginLabel.TabIndex = 3;
             this.TelefonoLoginLabel.Text = "Telefono";
             // 
@@ -86,81 +89,80 @@
             this.TelefonoLoginTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.TelefonoLoginTextBox.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TelefonoLoginTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.TelefonoLoginTextBox.Location = new System.Drawing.Point(472, 251);
-            this.TelefonoLoginTextBox.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.TelefonoLoginTextBox.Location = new System.Drawing.Point(354, 204);
+            this.TelefonoLoginTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TelefonoLoginTextBox.Name = "TelefonoLoginTextBox";
             this.TelefonoLoginTextBox.PasswordChar = '\0';
             this.TelefonoLoginTextBox.PlaceholderText = "Ingrese su numero de telefono";
             this.TelefonoLoginTextBox.SelectedText = "";
-            this.TelefonoLoginTextBox.Size = new System.Drawing.Size(440, 49);
+            this.TelefonoLoginTextBox.Size = new System.Drawing.Size(330, 40);
             this.TelefonoLoginTextBox.TabIndex = 6;
             // 
-            // guna2TextBox2
+            // ContrasenaTextBox
             // 
-            this.guna2TextBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.guna2TextBox2.BorderRadius = 8;
-            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox2.DefaultText = "";
-            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.Location = new System.Drawing.Point(472, 380);
-            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.guna2TextBox2.Name = "guna2TextBox2";
-            this.guna2TextBox2.PasswordChar = '\0';
-            this.guna2TextBox2.PlaceholderText = "Ingresa su contraseña";
-            this.guna2TextBox2.SelectedText = "";
-            this.guna2TextBox2.Size = new System.Drawing.Size(440, 49);
-            this.guna2TextBox2.TabIndex = 8;
+            this.ContrasenaTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ContrasenaTextBox.BorderRadius = 8;
+            this.ContrasenaTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ContrasenaTextBox.DefaultText = "";
+            this.ContrasenaTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.ContrasenaTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.ContrasenaTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ContrasenaTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ContrasenaTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ContrasenaTextBox.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContrasenaTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ContrasenaTextBox.Location = new System.Drawing.Point(354, 309);
+            this.ContrasenaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ContrasenaTextBox.Name = "ContrasenaTextBox";
+            this.ContrasenaTextBox.PasswordChar = '\0';
+            this.ContrasenaTextBox.PlaceholderText = "Ingresa su contraseña";
+            this.ContrasenaTextBox.SelectedText = "";
+            this.ContrasenaTextBox.Size = new System.Drawing.Size(330, 40);
+            this.ContrasenaTextBox.TabIndex = 8;
             // 
-            // label4
+            // ContrasenaLabel
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(49)))));
-            this.label4.Location = new System.Drawing.Point(465, 345);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 27);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Contraseña";
+            this.ContrasenaLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ContrasenaLabel.AutoSize = true;
+            this.ContrasenaLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContrasenaLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(49)))));
+            this.ContrasenaLabel.Location = new System.Drawing.Point(349, 280);
+            this.ContrasenaLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ContrasenaLabel.Name = "ContrasenaLabel";
+            this.ContrasenaLabel.Size = new System.Drawing.Size(97, 21);
+            this.ContrasenaLabel.TabIndex = 7;
+            this.ContrasenaLabel.Text = "Contraseña";
             // 
-            // guna2Button1
+            // IniciarSesionButton
             // 
-            this.guna2Button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.guna2Button1.BorderRadius = 8;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(79)))), ((int)(((byte)(217)))));
-            this.guna2Button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(471, 486);
-            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(440, 49);
-            this.guna2Button1.TabIndex = 9;
-            this.guna2Button1.Text = "Iniciar Sesión";
+            this.IniciarSesionButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.IniciarSesionButton.BorderRadius = 8;
+            this.IniciarSesionButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.IniciarSesionButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.IniciarSesionButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.IniciarSesionButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.IniciarSesionButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(79)))), ((int)(((byte)(217)))));
+            this.IniciarSesionButton.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IniciarSesionButton.ForeColor = System.Drawing.Color.White;
+            this.IniciarSesionButton.Location = new System.Drawing.Point(353, 395);
+            this.IniciarSesionButton.Name = "IniciarSesionButton";
+            this.IniciarSesionButton.Size = new System.Drawing.Size(330, 40);
+            this.IniciarSesionButton.TabIndex = 9;
+            this.IniciarSesionButton.Text = "Iniciar Sesión";
             // 
             // AdminLoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1348, 702);
-            this.Controls.Add(this.guna2Button1);
-            this.Controls.Add(this.guna2TextBox2);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(1011, 570);
+            this.Controls.Add(this.IniciarSesionButton);
+            this.Controls.Add(this.ContrasenaTextBox);
+            this.Controls.Add(this.ContrasenaLabel);
             this.Controls.Add(this.TelefonoLoginTextBox);
             this.Controls.Add(this.TelefonoLoginLabel);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InstruccionLabel);
             this.Controls.Add(this.SaludoLoginLabel);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminLoginForm";
             this.Text = "Login";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -172,11 +174,11 @@
         #endregion
 
         private System.Windows.Forms.Label SaludoLoginLabel;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label InstruccionLabel;
         private System.Windows.Forms.Label TelefonoLoginLabel;
         private Guna.UI2.WinForms.Guna2TextBox TelefonoLoginTextBox;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
-        private System.Windows.Forms.Label label4;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2TextBox ContrasenaTextBox;
+        private System.Windows.Forms.Label ContrasenaLabel;
+        private Guna.UI2.WinForms.Guna2Button IniciarSesionButton;
     }
 }
