e25019ccd50fe7d85793ca07259da453c3496fe5
Erick Caceres <erickcaceres1717@outlook.com>
2024-08-15 14:26:31 -0600

    Codificacion de los metodos en la clase ToolsFoms

--- SistemaBliss.UI.WinForms/ToolsForms.cs
+++ SistemaBliss.UI.WinForms/ToolsForms.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+///Referencias
+using System.Windows.Forms;
 
 namespace SistemaBliss.UI.WinForms
 {
-    internal class ToolsForms
+
+    public class ToolsForm
     {
+        public static long ObtenerIdGrid(DataGridView pDataGridView)
+    {
+        // Metodo para obtener el id de registro seleccionado en un DataGridView
+        try
+        {
+            long idTabla = 0;
+            // Obtener la PK del registro seleccionado
+            idTabla = Convert.ToInt64(pDataGridView.Rows[pDataGridView.CurrentRow.Index].Cells[0].Value.ToString());
+            return idTabla;
+        }
+        catch (Exception ex)
+        {
+            return 0;
+        }
+    }
+
+    public static DialogResult MessageBoxConfirmar()
+    {
+        return MessageBox.Show("¿Desea eliminar el registro seleccionado?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+    }
+}
+
+namespace ToolsForms
+{
+    public static class MyExtensions
+    {
+        public static bool ValidarControl(this ErrorProvider errorProvider, Control pControl, string pMensaje, string pRegex = null)
+        {
+            if (pControl is TextBox)
+            {
+                if (pControl.Text == null || pControl.Text.Trim() == string.Empty)
+                {
+                    errorProvider.SetError(pControl, pMensaje);
+                    return false; // dato invalido
+                }
+                if (pRegex != null && pRegex.Trim() != "")
+                {
+                    if (!System.Text.RegularExpressions.Regex.IsMatch(pControl.Text, pRegex))
+                    {
+                        errorProvider.SetError(pControl, "Formato invalido");
+                        return false; // formato invalido
+                    }
+                }
+            }
+            if (pControl is ComboBox && ((ComboBox)pControl).SelectedIndex <= 0)
+            {
+                errorProvider.SetError(pControl, pMensaje);
+                return false; // dato invalido
+            }
+            return true; // dato valido
+        }
     }
 }
