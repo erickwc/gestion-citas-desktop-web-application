2f2f41cec1069774a053782e91bde1f48a03f2e5
Michelle Figueroa <michellefigueroa335@outlook.com>
2024-09-16 14:43:58 -0600

    Creacion de los controladores de las secciones

---
+++ SistemaBliss.UI.AppWebMVC/Controllers/CitaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaBliss.UI.AppWebMVC.Controllers
+{
+    public class CitaController : Controller
+    {
+        // GET: Cita
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
