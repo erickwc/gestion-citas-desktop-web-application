9c28b4ae5c1f387d6372a123e7fcdb4296e9d043
Michelle Figueroa <michellefigueroa335@outlook.com>
2024-09-15 23:08:54 -0600

    se codificaron el display y funcionalidades en la capa Cita.EN

--- SistemaBliss.EN/Cita.cs
+++ SistemaBliss.EN/Cita.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaBliss.EN
 {
     public class Cita
     {
-
+        [Display(Name = "Id")]
         public int IdCita { get; set; }
+
+        [Display(Name = "Cliente")]
+        [Required(ErrorMessage = "Este campo es requerido")]
         public int IdCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este cacmpo es requerido")]
         public byte IdEstado { get; set; }
+
+
+        [Required(ErrorMessage = "Este cacmpo es requerido")]
         public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Este cacmpo es requerido")]
         public TimeSpan Hora { get; set; }
+
+        [Required(ErrorMessage = "Este cacmpo es requerido")]
         public TimeSpan TiempoTotal { get; set; }
+
+        [Required(ErrorMessage = "Este cacmpo es requerido")]
         public Decimal PagoTotal { get; set; }
     }
 }
